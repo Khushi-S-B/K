42381950b5f61592c8249e329861169671cd715a
Khushi-S-B <kb104242@gmail.com>
2019-08-01 14:00:38 +0530

    First commit

---
+++ areaofcirlce.py
@@ -0,0 +1,3 @@
+r=int(input("Enter the radius: "))
+ar=(22/7)*(r**2)
+print ("The area is", ar)
---
+++ areaoftro function.py
@@ -0,0 +1,6 @@
+def areaoftri():
+    base=2
+    height=4
+    area= 0.5*base*height
+    print(area)
+areaoftri()    
